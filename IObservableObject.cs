bf08d52739c5cbbc892b170623a2d1a6560b382d
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2023-01-11 12:55:04 +0100

    Agregar archivos de proyecto.

---
+++ IObservableObject.cs
@@ -0,0 +1,6 @@
+﻿namespace Personas
+{
+    internal interface IObservableObject
+    {
+    }
+}
---
+++ Modelo/Persona.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas.Modelo
+{
+    class Persona : ObservableObject
+    {
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { SetProperty(ref nombre, value); }
+        }
+
+        private String nacionalidad;
+
+        public String Nacionalidad
+        {
+            get { return nacionalidad; }
+            set { SetProperty(ref nacionalidad, value); }
+        }
+
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { SetProperty(ref edad, value); }
+        }
+
+        public Persona()
+        {
+
+        }
+
+    }
+}
